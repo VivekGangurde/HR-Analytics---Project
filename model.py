1663fc405dd0f7798ea72a21c2746f780de72556
Vivek Gangurde <114504162+VivekGangurde@users.noreply.github.com>
2024-05-06 00:20:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -156,7 +156,6 @@ def main():
     ans = round(100*ans,2)
     st.subheader('The probability of employee being Left the Company is {ans} %.')
 
-        
-    if __name__ == "__main__" :
-    main()
+if __name__ == "__main__" :
+main()
 
