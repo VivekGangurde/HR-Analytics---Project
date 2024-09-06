7dec76fc3c94449e2644d2af3782202564487bd1
Vivek Gangurde <114504162+VivekGangurde@users.noreply.github.com>
2024-09-06 14:25:37 +0530

    Update model.py

--- model.py
+++ model.py
@@ -152,7 +152,7 @@ def main():
         return inp
     
     df = input_features()
-    model = pickle.load(open('test.pkl','rb'))
+    model = pickle.load(open('model.pkl','rb'))
     ans = log_gs.predict_proba([df])[0][0]
     ans = round(100*ans,2)
     st.subheader('The probability of employee being Left the Company is {ans} %.')
