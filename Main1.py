a8357388527b85e046869d640d103b23ba05fa72
Vivek Gangurde <114504162+VivekGangurde@users.noreply.github.com>
2024-09-13 16:00:34 +0530

    Update Main1.py

--- Main1.py
+++ Main1.py
@@ -25,145 +25,154 @@ from sklearn.linear_model import LogisticRegression
 def main():
     st.header('Employee Turnover Predicton')
     st.subheader('(Please fill in the Employee details accordingly)')
-    def input_features() :
+def input_features() :
         st.sidebar.header('Sliders to change the variables.')
-        age = st.sidebar.slider('Age of employee', 18,65,30)
+        Age = st.sidebar.slider('Age of employee', 18,65,30)
         
-        dep = st.selectbox('Department', ['Sales', 'Human Resources', 'Research & Development'])
-        if dep == "Sales" or "Human Resources" :
-            dept = 0
+        Department = st.selectbox('Department', ['Sales', 'Human Resources', 'Research & Development'])
+        if Department == "Sales" or "Human Resources" :
+            Department = 0
         else :
-            dept = 1
+            Department = 1
         
-        distancefromhome = st.sidebar.slider('Distance from home(km)',0,50,5)
+DistanceFromHome = st.sidebar.slider('Distance from home(km)', 0,50,5)
         
-        efield = st.selectbox('Education Field',['Human Resources','Marketing','Technical Degree','Life Sciences','Medical', 'Other'])
-        if efield == "Human Resources" or "Marketing" or "Technical Degree" :
+EducationField = st.selectbox('Education Field',['Human Resources','Marketing','Technical Degree','Life Sciences','Medical', 'Other'])
+if EducationField == "Human Resources" or "Marketing" or "Technical Degree" :
             edu_field = 1
-        else :
+else :
             edu_field = 0
         
-        en = st.sidebar.slider('Number of people in the team', 1, 3000, 100)
-        if en <1495 :
+en = st.sidebar.slider('Number of people in the team', 1, 3000, 100)
+if en <1495 :
             employeenumber = 0
-        else:
+else:
             employeenumber = 1
             
-        envsat = st.selectbox('Environment Satisfaction',['Low', 'Medium','High','Very High'])
-        if envsat == "Low" :
+envsat = st.selectbox('Environment Satisfaction',['Low', 'Medium','High','Very High'])
+if envsat == "Low" :
             env_sat = 1
-        elif envsat == "Medium":
+elif envsat == "Medium":
             env_sat = 2
-        elif envsat == "High":
+elif envsat == "High":
             env_sat = 3
-        else :
+else :
             env_sat = 4
             
-        gend = st.selectbox('Gender', ['Male', 'Female'])
-        if gend == "Male" :
+gend = st.selectbox('Gender', ['Male', 'Female'])
+if gend == "Male" :
             gender = 1
-        else :
+else :
             gender = 0
             
-        involve = st.selectbox('Job Involvement',['Low', 'Medium','High','Very High'])
-        if involve == "Low":
+involve = st.selectbox('Job Involvement',['Low', 'Medium','High','Very High'])
+if involve == "Low":
             job_inv = 1
-        elif involve == "Medium":
+elif involve == "Medium":
             job_inv = 2
-        elif involve == "High":
+elif involve == "High":
             job_inv = 3
-        else :
+else :
             job_inv = 4
         
-        role = st.selectbox('Job Role', ['Research Director','Manager','Healthcare Representative','Manufacturing Director','Laboratory Technician','Research Scientist','Sales Executive','Human Resources','Sales Representative'])
-        if role == "Research Director" :
+role = st.selectbox('Job Role', ['Research Director','Manager','Healthcare Representative','Manufacturing Director','Laboratory Technician','Research Scientist','Sales Executive','Human Resources','Sales Representative'])
+if role == "Research Director" :
             jobrole = 0
-        elif role == "Manager" or "Healthcare Representative" or "Manufacturing Director":
+elif role == "Manager" or "Healthcare Representative" or "Manufacturing Director":
             jobrole = 1
-        elif role == "Laboratory Technician" :
+elif role == "Laboratory Technician" :
             jobrole = 3
-        elif role == "Research Scientist" or "Sales Executive" :
+elif role == "Research Scientist" or "Sales Executive" :
             jobrole = 2
-        elif role == "Human Resources" :
+elif role == "Human Resources" :
             jobrole = 4
-        else :
+else :
             jobrole = 5
             
-        jobsat = st.selectbox('Job Satisfaction', ['Low','Medium', 'High','Very High'])
-        if jobsat == "Low" :
+jobsat = st.selectbox('Job Satisfaction', ['Low','Medium', 'High','Very High'])
+if jobsat == "Low" :
             job_sat = 1
-        elif jobsat == "Medium" or "High" :
+elif jobsat == "Medium" or "High" :
             job_sat = 2
-        else :
+else :
             job_sat = 3
             
-        mar = st.selectbox('Marital Status',['Married','Divorced','Single'])
-        if mar == "Married" or "Divorced" :
+mar = st.selectbox('Marital Status',['Married','Divorced','Single'])
+if mar == "Married" or "Divorced" :
             mar_stat = 0
-        else :
+else :
             mar_stat = 1
             
-        income = st.sidebar.slider('Monthly Income', 0, 50000, 5000)
+income = st.sidebar.slider('Monthly Income', 0, 50000, 5000)
         
-        num_worked = st.number_input('Number of companies worked',0.,10.,step = 1.)
-        if num_worked <=4 :
+num_worked = st.number_input('Number of companies worked',0.,10.,step = 1.)
+if num_worked <=4 :
             num_com = 0
-        else :
+else :
             num_com = 1
             
-        otime = st.selectbox("Does the employee work overtime?",['No', 'Yes'])
-        if otime == "No" :
+otime = st.selectbox("Does the employee work overtime?",['No', 'Yes'])
+if otime == "No" :
             overtime = 0
-        else :
+else :
             overtime = 1
             
-        sol = st.selectbox('Stock of employee in the company', ["No stocks", "Moderate stocks", "Lots of stocks"])
-        if sol == "No stocks" :
+sol = st.selectbox('Stock of employee in the company', ["No stocks", "Moderate stocks", "Lots of stocks"])
+if sol == "No stocks" :
             stocks = 0 
-        elif sol == "Moderate stocks" :
+elif sol == "Moderate stocks" :
             stocks = 1 
-        else :
+else :
             stocks = 2
             
-        total_exp = st.number_input("Total experience of employee",0.,45.,step = 1.)
+total_exp = st.number_input("Total experience of employee",0.,45.,step = 1.)
         
-        training = st.number_input("Number of times employee did training",0.,9., step = 1.)
+training = st.number_input("Number of times employee did training",0.,9., step = 1.)
         
-        years_com = st.number_input("Number of years spent in company",0.,50., step = 1.)
+years_com = st.number_input("Number of years spent in company",0.,50., step = 1.)
         
-        years_role = st.number_input("Number of years in current role",0.,30., step = 1.)
+years_role = st.number_input("Number of years in current role",0.,30., step = 1.)
         
-        ym = st.number_input("Years with current manager",0.,20., step = 1.)
-        if ym == 0 :
+ym = st.number_input("Years with current manager",0.,20., step = 1.)
+if ym == 0 :
             years_man = 0 
-        elif ym < 12 :
+elif ym < 12 :
             years_man = 1
-        else :
+else :
             years_man = 2
             
-        WLB = st.selectbox("WorkLifeBalance", ["Poor","Bad","Good", "Better", "Best"])
-        if WLB == "Poor" :
+WLB = st.selectbox("WorkLifeBalance", ["Poor","Bad","Good", "Better", "Best"])
+if WLB == "Poor" :
             WLB_Score = 1
-        elif WLB == "Bad" :
+elif WLB == "Bad" :
             WLB_Score = 2
-        elif WLB == "Good" :
+elif WLB == "Good" :
             WLB_Score =3
-        elif WLB == "Better":
+elif WLB == "Better":
             WLB_Score = 4
-        else :
+else :
             WLB_Score = 5
             
-      inp = [age,DailyRate,DistanceFromHome,Education,EmployeeCount,EmployeeNumber,EnvironmentSatisfaction,Gender,HourlyRate,JobInvolvement,JobLevel,JobSatisfaction,MonthlyIncome,MonthlyRate,NumCompaniesWorked,OverTime,PercentSalaryHike,PerformanceRating,RelationshipSatisfaction,StockOptionLevel,TotalWorkingYears,TrainingTimesLastYear,WorkLifeBalance, YearsAtCompany,YearsInCurrentRole,YearsSinceLastPromotion,YearsWithCurrManager,BusinessTravel_Non-Travel,BusinessTravel_Travel_Frequently,BusinessTravel_Travel_Rarely,Department_Human Resources,Department_Research & Development,Department_Sales,EducationField_Human Resources,EducationField_Life Sciences,EducationField_Marketing,EducationField_Medical,EducationField_Other,EducationField_Technical Degree,JobRole_Healthcare Representative,JobRole_Human Resources,JobRole_Laboratory Technician,JobRole_Manager,JobRole_Manufacturing Director,JobRole_Research Director,JobRole_Research Scientist,JobRole_Sales Executive,JobRole_Sales Representative,MaritalStatus_Divorced,MaritalStatus_Married,MaritalStatus_Single]
-        
-        
-        
-        return inp
-    
-    df = input_features()
-    log_gs = pickle.load(open('model.pkl','rb'))
-    ans = log_gs.predict_proba([df])[0][0]
-    ans = round(100*ans,2)
-    st.subheader('The probability of employee being Left the Company is {ans} %.')
+            inp =  [Age,DailyRate,DistanceFromHome,Education,employeenumber,EnvironmentSatisfaction,
+                    Gender,HourlyRate,Jobinvolvement,JoblLevel,JobSatisfaction,MonthlyIncome,MonthlyRate,
+                    NumCompaniesWorked,OverTime,PercentSalaryHike,PerformanceRating,RelationshipSatisfaction,
+                    StockOptionLevel,TotalWorkingYears,TrainingTimesLastYear,WorkLifeBalance,YearsAtCompany,
+                    YearsInCurrentRole,YearsSinceLastPromotion,YearsWithCurrManger,BusinessTravel_Non-Travel,
+                    BusinessTravel_Travel_Frequently,BusinessTravel_Travel_Rarely,Department_HumanResources,
+                    Deparment_Reseearch & Development,Department_Sales,EducationField_HumanResources,
+                    Educationfield_LifeSciences,Educationfield_Marketing,EducationField_Medical,EducationField_Other,
+                    EducationField_TechnicalDegree,JobRole_HealthcareRepresentative,JobRole_HumanResources,
+                    JobRole_LaboratoryTechnician,JobRole_Manager,JobRole_ManufacturingDirector,JobRole_ResearchDirector,
+                    JobRole_ResearchScientist,JobRole_SalesExecutive,JobRole_SalesRepresentative,MaritalStatus_Divorced,
+                    MaritalStatus_Married,MaritalStatus_Single] 
+            
+return inp
+
+df = input_features()
+log_gs = pickle.load(open('model.pkl','rb'))
+ans = log_gs.predict_proba([df])[0][0]
+ans = round(100*ans,2)
+st.subheader('The probability of employee being Left the Company is {ans} %.')
     
 if __name__ == "__main__" :
     main()
