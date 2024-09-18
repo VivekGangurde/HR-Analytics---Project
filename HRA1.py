da2efb060db6c26ca0681471b82d1407198bb825
Vivek Gangurde <114504162+VivekGangurde@users.noreply.github.com>
2024-09-18 12:30:56 +0530

    Update HRA1.py

--- HRA1.py
+++ HRA1.py
@@ -25,8 +25,8 @@ def main():
     st.subheader('(Please fill in the Employee details accordingly)')
     def input_features() :
         st.sidebar.header('Sliders to change the variables.')
-        st.sidebar.slider('Age of employee', 18,65,30)
-        
+        age=st.selectbox('Age of employee', 18,65,30)
+       
         dep = st.selectbox('Department', ['Sales', 'Human Resources', 'Research & Development'])
         if dep == "Sales" or "Human Resources" :
             dept = 0
