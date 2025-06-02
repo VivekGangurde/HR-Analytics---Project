52c34b4a3f6ec0cfaa1e10ca812ae1a5e8a43c52
Vivek Gangurde <114504162+VivekGangurde@users.noreply.github.com>
2025-06-02 12:17:47 +0530

    Update Fast API.py

--- Fast API.py
+++ Fast API.py
@@ -64,7 +64,7 @@ async def func(Input:user_input):
                                             
     return json.dumps(output)
 if __name__ == "__main__":
-    uvicorn.run("hr_analytics_api:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    uvicorn.run("hr_analytics_api:app", host="127.0.0.1", port=8000,  reload=True)
 
 
 # In[ ]:
